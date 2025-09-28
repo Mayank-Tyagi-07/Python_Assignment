0434f7e9f7df13fd02779864e58b35353f2e61e6
Mayank-Tyagi-07 <mayanktyagi9756@gmail.com>
2025-09-29 03:02:13 +0530

    Uploading Assignment 1

---
+++ Assignment_1/Task_1.py
@@ -0,0 +1,24 @@
+# Performing Basic Mathematical Operations
+
+#Taking input from the user
+
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+
+print("\n\n")
+
+#performing addition
+Addition = int(first_number) + int(second_number)
+print("Addition: ",Addition)
+
+#performing substraction
+Substraction = int(first_number) - int(second_number)
+print("Substraction: ",Substraction)
+
+#performing multiplication
+Multiplication = int(first_number) * int(second_number)
+print("Multiplication: ",Multiplication)
+
+#performing division
+Division = int(first_number) / int(second_number)
+print("Division: ",Division)
